182bf3074c5fff90db40de38df9f1ad28c47b485
rehanulhb <rehanulh@gmail.com>
2025-03-10 20:22:16 +0100

    Working on Setting up Eslint, prettier and Mongoose Connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+export default app;
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+
 
 async function main() {
-    await mongoose.connect(process.env.DATABASE_URL);
+
+  try{
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  }
+  catch(err){
+    console.log(err)
+  }
+
+    
   
     
   }
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
