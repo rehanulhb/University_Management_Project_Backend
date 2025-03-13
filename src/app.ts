232a6eef7881844f7a6c577dd3bcc3fdc812412e
rehanulhb <rehanulh@gmail.com>
2025-03-13 21:26:42 +0100

    added some code to Restablish connection to Mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,8 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app: Application = express()
-
-
-app.use(express.json());
-app.use(cors())
-
-app.get('/', (req: Request, res: Response) => {
-  const a=10;
-  res.send(a)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-
-export default app;
-
 
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,11 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
-
-
+const mongoose = require('mongoose');
 
 async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  try{
-    await mongoose.connect(config.database_url as string);
+  
+}
 
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
-  }
-  catch(err){
-    console.log(err)
-  }
- }
- main()
-
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
