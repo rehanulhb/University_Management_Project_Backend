a5efba77f2239756b04a86c7692a6c4cb24a01b5
rehanulhb <rehanulh@gmail.com>
2025-03-15 02:09:05 +0100

    Added Some code for basic Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  eslintPluginPrettierRecommended,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   {
     rules: {
-      eqeqeq: "off",
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-      "no-unused-expressions": "error",
-      "no-undef": "error"
-      
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-unused-expressions': 'error',
+      'no-undef': 'error',
+      'no-console': 'warn',
     },
-    
+    globals: {
+      process: 'readonly',
+    },
   },
-  
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
-
+]
--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,8 @@ const app: Application = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req: Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-
-export default app;
+export default app
